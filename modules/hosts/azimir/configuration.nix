632e665e7b1de8ea5e3959ce7b49ee5122df2e58
Micah Johnson <mjohnson@cltexam.com>
2026-03-29 18:25:02 -0400

    sys: add tuned and upower for noctalia integration

--- modules/hosts/azimir/configuration.nix
+++ modules/hosts/azimir/configuration.nix
@@ -65,6 +65,9 @@
 
       services.fwupd.enable = true;
 
+      services.power-profiles-daemon.enable = true;
+      services.upower.enable = true;
+
       hardware.graphics.enable = true;
       hardware.bluetooth = {
         enable = true;
