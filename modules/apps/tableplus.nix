4c2f7d9ac3d32b55917d8eeabf4a88a8aed52b38
Micah Johnson <mjohnson@cltexam.com>
2026-04-29 11:20:25 -0400

    home: fix tableplus theme

--- modules/apps/tableplus.nix
+++ modules/apps/tableplus.nix
@@ -20,12 +20,17 @@
         pkgs.appimageTools.wrapType2 {
           inherit pname version src;
 
-          # Electron ignores XCURSOR_THEME and reads the cursor theme name from GTK
-          # settings, which aren't available inside the FHS sandbox. Expanding the
-          # cursor search path lets it find ~/.icons/default/index.theme, which
-          # home-manager sets up to inherit from the stylix cursor theme.
+          # Several tweaks to fix GTK/system theming issues with cursor and colors.
           profile = ''
             export XCURSOR_PATH="$HOME/.icons:$HOME/.local/share/icons:/usr/share/icons:/usr/share/pixmaps"
+            export XDG_DATA_DIRS="$HOME/.nix-profile/share:$HOME/.local/share:''${XDG_DATA_DIRS:-/usr/share}"
+            if [ -r "$HOME/.config/gtk-3.0/settings.ini" ]; then
+              _gtk_theme=$(/usr/bin/awk -F= '/^gtk-theme-name[[:space:]]*=/ { gsub(/^[[:space:]]+|[[:space:]]+$/, "", $2); print $2; exit }' "$HOME/.config/gtk-3.0/settings.ini" 2>/dev/null)
+              if [ -n "$_gtk_theme" ]; then
+                export GTK_THEME="$_gtk_theme"
+              fi
+              unset _gtk_theme
+            fi
           '';
 
           extraInstallCommands = ''
