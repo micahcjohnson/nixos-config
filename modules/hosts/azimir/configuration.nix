98371a21ed228fb46254b70071f23ca45bd98932
Micah Johnson <mjohnson@cltexam.com>
2026-05-06 11:44:03 -0400

    fix webcam autoload failure

--- modules/hosts/azimir/configuration.nix
+++ modules/hosts/azimir/configuration.nix
@@ -16,6 +16,7 @@
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
       boot.kernelPackages = pkgs.linuxPackages_latest;
+      boot.kernelModules = [ "uvcvideo" ];
 
       networking.hostName = "azimir";
       networking.networkmanager.enable = true;
