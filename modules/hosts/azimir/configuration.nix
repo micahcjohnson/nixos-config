84ada5ec065091c5875e94fa5f07a3dffbf4ee15
Micah Johnson <mjohnson@cltexam.com>
2026-05-17 19:10:53 -0400

    sys: pin kernel to 7.0.5 until BT patch is available

--- modules/hosts/azimir/configuration.nix
+++ modules/hosts/azimir/configuration.nix
@@ -15,7 +15,10 @@
 
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
-      boot.kernelPackages = pkgs.linuxPackages_latest;
+      # Pinned to nixpkgs-kernel (rev da5ad66) = kernel 7.0.5. 7.0.8 has a
+      # btmtk/btusb regression that breaks MT7925 Bluetooth (WMT func ctrl
+      # -22). Rest of the system still tracks the main nixpkgs input.
+      boot.kernelPackages = inputs.nixpkgs-kernel.legacyPackages.${pkgs.stdenv.hostPlatform.system}.linuxPackages_latest;
       boot.kernelModules = [ "uvcvideo" ];
 
       networking.hostName = "azimir";
