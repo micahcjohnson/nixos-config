25f588aee361bba2a03fd8c3894c3092ef503c82
Micah Johnson <mjohnson@cltexam.com>
2026-06-01 07:26:15 -0400

    flake: update inputs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     nixos-hardware.url = "github:nixos/nixos-hardware";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
